ec1d6fad1d197de9737e936dd49ec4f328776648
Elisa <rimbanoelisa@gmail.com>
2018-05-29 19:11:13 +0200

    v finale

--- app.js
+++ app.js
@@ -2,13 +2,19 @@
 function changenamebutton1() // no ';' here
 {
     var elem = document.getElementById("button1");
-    if (elem.textContent = "See More") elem.textContent = "See Less";
-    else elem.textContent = "See More";
+    if (elem.textContent === "See More") {
+        elem.textContent = "See Less";
+    } else {
+        elem.textContent = "See More";
+    }
 }
 
 function changenamebutton2() // no ';' here
 {
     var elem = document.getElementById("button2");
-    if (elem.textContent = "See More") elem.textContent = "See Less";
-    else elem.textContent = "See More";
+    if (elem.textContent === "See More") {
+        elem.textContent = "See Less";
+    } else {
+        elem.textContent = "See More";
+    }
 }

8b45a23193aa98e6cbfd04eb7b15563c60d7509e
Elisa <rimbanoelisa@gmail.com>
2018-05-29 18:48:48 +0200

    see less première etape js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 
+function changenamebutton1() // no ';' here
+{
+    var elem = document.getElementById("button1");
+    if (elem.textContent = "See More") elem.textContent = "See Less";
+    else elem.textContent = "See More";
+}
+
+function changenamebutton2() // no ';' here
+{
+    var elem = document.getElementById("button2");
+    if (elem.textContent = "See More") elem.textContent = "See Less";
+    else elem.textContent = "See More";
+}
